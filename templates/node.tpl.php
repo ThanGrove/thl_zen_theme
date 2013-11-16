65c33ab136240d1d3538ab7731a007d17f9e14c9
ThanGrove <than@virginia.edu>
2013-11-16 08:47:37 -0500

    Cleaning up node templates

--- templates/node.tpl.php
+++ templates/node.tpl.php
@@ -20,7 +20,7 @@
     <header>
         <?php print render($title_prefix); ?>
         <?php if (!$page && $title): ?>
-          <h2<?php print $title_attributes; ?>><a href="<?php print $node_url; ?>"><?php print $title; ?></a> HERE</h2>
+          <h2<?php print $title_attributes; ?>><a href="<?php print $node_url; ?>"><?php print $title; ?></a></h2>
         <?php endif; ?>
         <?php print render($title_suffix); ?>
   
